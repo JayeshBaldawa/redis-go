2282b46dad75c7e099b54c8ddf45e8ec498e9f98
VenomousJ <jayesh2011baldawa@gmail.com>
2024-04-21 23:54:22 +0530

    continue to prev commit

--- app/models/parser.go
+++ app/models/parser.go
@@ -1,5 +1,15 @@
 package models
 
+import (
+	"regexp"
+)
+
+var RegexFullResyncPattern *regexp.Regexp
+
+func init() {
+	RegexFullResyncPattern = regexp.MustCompile(REGEX_FULLRESYNC)
+}
+
 const (
 	SIMPLE = "+"
 	ERROR  = "-"
@@ -8,10 +18,13 @@ const (
 )
 
 const (
-	STR_WRAPPER = "\r\n"
-	FIRST_BYTE  = "$"
-	REPLICATION = "# Replication\nrole:%s\nmaster_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\nmaster_repl_offset:0\n"
-	FULLRESYNC  = "FULLRESYNC"
+	STR_WRAPPER    = "\r\n"
+	FIRST_BYTE     = "$"
+	REPLICATION    = "# Replication\nrole:%s\nmaster_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\nmaster_repl_offset:0\n"
+	FULLRESYNC     = "FULLRESYNC"
+	EMPTY_RDB_FILE = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
+	ACK_RESP       = "ACK"
+	OFFSET         = "%d"
 )
 
 const (
@@ -26,6 +39,7 @@ const (
 	REPLCONF_CAPA        = "capa"
 	REPLCONF_PYSYNC2     = "psync2"
 	PYSNC                = "psync"
+	GETACK               = "getack"
 )
 
 const (
@@ -33,4 +47,13 @@ const (
 	PX   = "px"   // Milliseconds
 	EXAT = "exat" // Unix timestamp in seconds
 	PXAT = "pxat" // Unix timestamp in milliseconds
+)
+
+type CommandOutput struct {
+	ComamndName string
+	Response    string
+}
+
+const (
+	REGEX_FULLRESYNC = `FULLRESYNC\s(?P<replid>\w+)\s(?P<offset>\d+)`
 )
