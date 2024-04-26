e60090b10bac39923dc24925807b7d0526c95a58
VenomousJ <jayesh2011baldawa@gmail.com>
2024-04-26 15:52:17 +0530

    add ack support | refactor code | processed bytes support as well

--- app/models/parser.go
+++ app/models/parser.go
@@ -1,15 +1,5 @@
 package models
 
-import (
-	"regexp"
-)
-
-var RegexFullResyncPattern *regexp.Regexp
-
-func init() {
-	RegexFullResyncPattern = regexp.MustCompile(REGEX_FULLRESYNC)
-}
-
 const (
 	SIMPLE = "+"
 	ERROR  = "-"
@@ -23,8 +13,6 @@ const (
 	REPLICATION    = "# Replication\nrole:%s\nmaster_replid:8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb\nmaster_repl_offset:0\n"
 	FULLRESYNC     = "FULLRESYNC"
 	EMPTY_RDB_FILE = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"
-	ACK_RESP       = "ACK"
-	OFFSET         = "%d"
 )
 
 const (
@@ -40,6 +28,7 @@ const (
 	REPLCONF_PYSYNC2     = "psync2"
 	PYSNC                = "psync"
 	GETACK               = "getack"
+	ACK_RESP             = "ACK"
 )
 
 const (
@@ -53,7 +42,3 @@ type CommandOutput struct {
 	ComamndName string
 	Response    string
 }
-
-const (
-	REGEX_FULLRESYNC = `FULLRESYNC\s(?P<replid>\w+)\s(?P<offset>\d+)`
-)
